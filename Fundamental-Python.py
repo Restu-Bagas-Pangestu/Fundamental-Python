5347a4e6fda9bb4ff45d9ebde041e9306da32528
RBAGASPANGES <restu.bagas.pangestu@gmail.com>
2023-07-23 21:37:27 +0700

    program perulangan for

--- Fundamental-Python.py
+++ Fundamental-Python.py
@@ -14,3 +14,30 @@ for jumlah_buku_yang_sudah_dibaca in range (1,jumlah_buku+1):
     print(f'buku ke {jumlah_buku_yang_sudah_dibaca} sudah di baca')
 print(jumlah_buku_yang_sudah_dibaca)
 print(f'Jumlah buku yang sudah di baca {jumlah_buku_yang_sudah_dibaca}')
+
+""""
+program perulangan membaca buku dengan while
+"""
+
+jumlah_buku = 10
+print('ibu berkata, "baca semua bukumu"')
+
+jumlah_buku_yang_sudah_dibaca = 0
+
+while jumlah_buku_yang_sudah_dibaca < jumlah_buku:
+    print('baca 1 buku yang belum dibaca')
+    jumlah_buku_yang_sudah_dibaca = jumlah_buku_yang_sudah_dibaca+1
+print(f'buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca')
+print(f'Jumlah buku yang sudah di baca {jumlah_buku_yang_sudah_dibaca}')
+
+jumlah_buku = 10
+print('ibu berkata, "baca semua bukumu"')
+
+jumlah_buku_yang_sudah_dibaca = 0
+
+while jumlah_buku_yang_sudah_dibaca < jumlah_buku:
+    jumlah_buku_yang_sudah_dibaca = jumlah_buku_yang_sudah_dibaca+1
+    print(f'buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca')
+
+print(f'buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca')
+print(f'Jumlah buku yang sudah di baca {jumlah_buku_yang_sudah_dibaca}')
