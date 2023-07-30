bede6cfd2cba3bcdfac6754aeba76ec08da6f5dc
RBAGASPANGES <restu.bagas.pangestu@gmail.com>
2023-07-31 00:50:40 +0700

    program perulangan for dan while

--- dasar_sintaksis_tipe_data_list.py
+++ dasar_sintaksis_tipe_data_list.py
@@ -85,3 +85,45 @@ daftar_buku = ['seven habits','How to influence people', 'first things first','4
 del daftar_buku[0::2]#start:end
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
+
+print('\nmembuat list baru')
+daftar_buku = ['seven habits','How to influence people', 'first things first','4DX']
+daftar_buku_baru=daftar_buku
+for i in range(0, len(daftar_buku_baru)):
+    print(daftar_buku[i])
+print('\nmembuat list baru')
+del daftar_buku[:]
+for i in range(0, len(daftar_buku_baru)):
+    print(daftar_buku_baru[i])
+
+print('\nmembuat list baru dengan comprehension')
+daftar_buku = ['seven habits','How to influence people', 'first things first','4DX']
+daftar_buku_baru=daftar_buku [:] #list comprehension untuk membuat list yang terpisah (sama kaya .copy())
+for i in range(0, len(daftar_buku_baru)):
+    print(daftar_buku[i])
+print('\nmembuat list baru')
+del daftar_buku[:]
+for i in range(0, len(daftar_buku_baru)):
+    print(daftar_buku_baru[i])
+
+print('\nmembuat list baru dengan comprehension: ganjil')
+daftar_buku = ['1 seven habits','2 How to influence people', '3 first things first','4 4DX']
+daftar_buku_baru=daftar_buku [0::2] #start:stop
+for i in range(0, len(daftar_buku_baru)):
+    print(daftar_buku_baru[i])
+
+print('\nmembuat list baru dengan comprehension: genap')
+daftar_buku = ['1 seven habits','2 How to influence people', '3 first things first','4 4DX']
+daftar_buku_baru=daftar_buku [1::2] #start:stop
+for i in range(0, len(daftar_buku_baru)):
+    print(daftar_buku_baru[i])
+
+print('\nmembuat list baru dengan comprehension: buang diujung')
+daftar_buku = ['1 seven habits','2 How to influence people', '3 first things first','4 4DX']
+daftar_buku_baru=daftar_buku[0:-1:2] #start:stop
+for i in range(0, len(daftar_buku_baru)):
+    print(daftar_buku_baru[i])
+
+print('\nmembuat list baru dengan comprehension: ganjil')
+daftar_buku = ['1 seven habits','2 How to influence people', '3 first things first','4 4DX']
+print(daftar_buku [0::2]) #start:stop
