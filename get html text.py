4eb0e3e7363fb39a27cb4fb41e5bd13d82d871ef
RBAGASPANGES <restu.bagas.pangestu@gmail.com>
2023-10-17 00:55:27 +0700

    finish

--- get html text.py
+++ get html text.py
@@ -10,4 +10,15 @@ result = requests.get(website)
 content= result.text
 
 soup = BeautifulSoup(content, 'lxml')
-print(soup.prettify())
+#print(soup.prettify())
+
+box = soup.find('article', class_ = 'main-article') #kasih underscore
+
+title= soup.find('h1').get_text()
+print(title)
+
+transcript=soup.find('div',class_='full-script').get_text(strip=True, separator=' ') #separator untuk spasinya pakai apa, strip=untuk spasi
+print(transcript)
+
+with open (f'{titanic}.txt', 'w') as file:
+    file.write(transcript)
